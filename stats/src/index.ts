6a3ccb02eb2da08d7bb602aeeaa9dd309567ef5c
Frank Huff <paintersmechanic@gmail.com>
2019-10-29 16:45:43 -0700

    added enum and utils

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,22 +1,19 @@
-import fs from 'fs';
+import { CsvFileReader } from './CsvFileReader';
+import { MatchResult } from './MatchResults';
 
-const matches = fs
-  .readFileSync('football.csv', {
-    encoding: 'utf-8'
-  })
-  .split('\n')
-  .map((row: string): string[] => {
-    return row.split(',');
-  });
+const reader = new CsvFileReader('football.csv');
+reader.read();
+
+
 
 let manUnitedWins = 0;
 
-for (let match of matches) {
-  if (match[1] === 'Man United' && match[5] === 'H') {
+for (let match of reader.data) {
+  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
     manUnitedWins++;
-  } else if (match[2] === 'Man United' && match[5] === 'A') {
+  } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
     manUnitedWins++;
   }
 }
 
-console.log(`Man united won ${manUnitedWins} games.`)
+console.log(`Man united won ${manUnitedWins} games.`);
