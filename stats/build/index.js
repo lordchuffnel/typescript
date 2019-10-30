56b488d8bbdf8b30cc00b548ed7b179c00966604
Frank Huff <paintersmechanic@gmail.com>
2019-10-29 18:06:16 -0700

    composition vs inheritance

--- stats/build/index.js
+++ stats/build/index.js
@@ -1,24 +1,18 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-var fs_1 = __importDefault(require("fs"));
-var matches = fs_1.default
-    .readFileSync('football.csv', {
-    encoding: 'utf-8'
-})
-    .split('\n')
-    .map(function (row) {
-    return row.split(',');
-});
+var Matchreader_1 = require("./Matchreader");
+var MatchResults_1 = require("./MatchResults");
+var CsvFileReader_1 = require("./CsvFileReader");
+var csvFileReader = new CsvFileReader_1.CsvFileReader('football.csv');
+var matchReader = new Matchreader_1.MatchReader(csvFileReader);
+matchReader.load();
 var manUnitedWins = 0;
-for (var _i = 0, matches_1 = matches; _i < matches_1.length; _i++) {
-    var match = matches_1[_i];
-    if (match[1] === 'Man United' && match[5] === 'H') {
+for (var _i = 0, _a = matchReader.matches; _i < _a.length; _i++) {
+    var match = _a[_i];
+    if (match[1] === 'Man United' && match[5] === MatchResults_1.MatchResult.HomeWin) {
         manUnitedWins++;
     }
-    else if (match[2] === 'Man United' && match[5] === 'A') {
+    else if (match[2] === 'Man United' && match[5] === MatchResults_1.MatchResult.AwayWin) {
         manUnitedWins++;
     }
 }
