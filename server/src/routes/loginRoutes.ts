672811272c3e14f471c931ed0d9c4333401d4885
Frank Huff <fs-j@live.com>
2019-11-01 14:41:06 -0700

    f

--- server/src/routes/loginRoutes.ts
+++ server/src/routes/loginRoutes.ts
@@ -1,7 +1,17 @@
-import { Router, Request, Response } from 'express';
+import { Router, Request, Response, NextFunction } from 'express';
 
 interface RequestWithBody extends Request {
   body: { [key: string]: string | undefined };
+}
+
+function requireAuth(req: Request, res: Response, next: NextFunction): void {
+  if (req.session && req.session.logggedIn) {
+    next();
+    return;
+  }
+
+  res.status(403);
+  res.send('not permitted');
 }
 
 const router = Router();
@@ -41,8 +51,24 @@ router.get('/', (req: Request, res: Response) => {
         <div>You are logged in</div>
         <a href="/logout:>Logout</a>
       </div>
-    `)
+    `);
+  } else {
+    res.send(`
+      <div>
+        <div>You are not logged in</div>
+        <a href="/login:>Login</a>
+      </div>
+    `);
   }
+});
+
+router.get('/logout', (req: Request, res: Response) => {
+  req.session = undefined;
+  res.redirect('/');
+});
+
+router.get('/protected', requireAuth,  (req: Request, res: Response) => {
+  res.send("welcome, you made it");
 })
 
 export { router };
